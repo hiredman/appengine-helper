f53af2b39c38f086f6b1cdee9b1124be8aea4029
Kevin Downey <redchin@gmail.com>
2010-02-28 13:01:04 -0800

    fix readme, etc

--- src/hiredman/datastore.clj
+++ src/hiredman/datastore.clj
@@ -1,32 +1,50 @@
 (ns hiredman.datastore
-  (:import (com.google.appengine.api.datastore DatastoreServiceFactory Query Query$FilterOperator Entity)))
+  (:import (com.google.appengine.api.datastore DatastoreServiceFactory 
+					       Query Query$FilterOperator
+					       Entity)))
 
-(def #^{:private true} datastore-service (DatastoreServiceFactory/getDatastoreService)) 
+(def #^{:private true} 
+     datastore-service (DatastoreServiceFactory/getDatastoreService)) 
 
 (defn- entity->map [entity]
   (with-meta
     (into {} (map #(vector (keyword (key %)) (val %)) (.getProperties entity)))
-    {:kind (keyword (.getKind entity))
-     :key (.getKey entity)
-     :entity entity}))
+    {::kind (keyword (.getKind entity))
+     ::key (.getKey entity)
+     ::entity entity}))
 
-(defn create [map]
-  (let [kind (name (:kind (meta map)))
-        entity (Entity. kind)]
-    (doseq [[name- value] map]
-      (.setProperty entity (name name-) value))
-    (.put datastore-service entity)
-    (entity->map entity)))
+(defn create
+  ([kind map’]
+     (create (vary-meta map’ assoc ::kind kind)))
+  ([map’]
+     (let [kind (name (::kind (meta map’)))
+	   entity (Entity. kind)]
+       (doseq [[name’ value] map’]
+	 (.setProperty entity (name name’) value))
+       (.put datastore-service entity)
+       (entity->map entity))))
 
-(defn delete [map]
-  (.delete datastore-service [(:key (meta map))])
+(defn delete [map’]
+  (.delete datastore-service [(::key (meta map’))])
   nil)
 
-(defn update [map]
-  (entity->map (reduce #(.setProperty %1 (name (first %2)) (second %2)) (:entity (meta map)) map)))
+(defn update [map’]
+  (entity->map
+   (reduce
+    #(.setProperty %1 (name (first %2)) (second %2))
+    (::entity (meta map’)) map’)))
+
+(defn update [map’]
+  (delete map’)
+  (create map’))
+
+#_(defn exec [query]
+  (-> query (#(.prepare datastore-service %))
+      .asIterable ((partial map entity->map))))
 
 (defn exec [query]
-  (-> query (#(.prepare datastore-service %)) .asIterable ((partial map entity->map))))
+  (-> datastore-service (.prepare query) .asIterable
+      ((partial map entity->map))))
 
 (def #^{:private true} operators
   {'= Query$FilterOperator/EQUAL
